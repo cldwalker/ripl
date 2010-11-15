57ec5ef5e9e536d0f54cb3179e796f4b00afadba
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-15 09:05:32 -0500

    add shell docs, cleaning up its api

--- lib/ripl/shell.rb
+++ lib/ripl/shell.rb
@@ -17,6 +17,7 @@ class Ripl::Shell
     @irbrc, @line = @options[:irbrc], 1
   end
 
+  # Loops shell until user exits
   def loop
     before_loop
     catch(:ripl_exit) { while(true) do; loop_once; end }
@@ -25,56 +26,73 @@ class Ripl::Shell
 
   def config; Ripl.config; end
 
+  # Runs through one loop iteration: gets input, evals and prints result
+  def loop_once
+    @error_raised = nil
+    @input = get_input
+    throw(:ripl_exit) if !@input || @input == 'exit'
+    eval_input(@input)
+    print_result(@result)
+  end
+
+  # Sets @result to result of evaling input and print unexpected errors
+  def eval_input(input)
+    @result = loop_eval(input)
+    eval("_ = Ripl.shell.result", @binding)
+  rescue Exception => e
+    @error_raised = true
+    print_eval_error(e)
+  ensure
+    @line += 1
+  end
+
   module API
+    # Sets up shell before looping by loading ~/.irbrc. Can be extended to
+    # initialize plugins and their instance variables.
     def before_loop
       Ripl::Runner.load_rc(@irbrc) if @irbrc
     end
 
-    def loop_once
-      @error_raised = nil
-      @input = get_input
-      throw(:ripl_exit) if !@input || @input == 'exit'
-      eval_input(@input)
-      print_result(@result)
-    end
-
+    # @return [String, nil] Prints #prompt and returns input given by user
     def get_input
       print prompt
       $stdin.gets.chomp
     end
 
+    # @return [String]
     def prompt
-      @options[:prompt].respond_to?(:call) ? @options[:prompt].call : @options[:prompt]
+      @options[:prompt].respond_to?(:call) ? @options[:prompt].call :
+        @options[:prompt]
     end
 
-    def eval_input(input)
-      @result = loop_eval(input)
-      eval("_ = Ripl.shell.result", @binding)
-    rescue Exception => e
-      @error_raised = true
-      print_eval_error(e)
-    ensure
-      @line += 1
-    end
-
+    # Evals user input using @binding, @name and @line
     def loop_eval(str)
       eval(str, @binding, "(#{@name})", @line)
     end
 
+    # Prints error formatted by #format_error to STDERR. Could be extended to
+    # handle certain exceptions.
+    # @param [Exception]
     def print_eval_error(err)
       warn format_error(err)
     end
 
+    # Prints result using #format_result
     def print_result(result)
       puts(format_result(result)) unless @error_raised
     end
 
+    # Formats errors raised by eval of user input
+    # @param [Exception]
+    # @return [String]
     def format_error(err); Ripl::Runner.format_error(err); end
 
+    # @return [String] Formats result using result_prompt
     def format_result(result)
       @options[:result_prompt] + result.inspect
     end
 
+    # Called after shell finishes looping.
     def after_loop; end
   end
   include API
