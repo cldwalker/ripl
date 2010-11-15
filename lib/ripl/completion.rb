f8a567391d853f5f4f2047ad7df014a19478362d
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-14 19:56:30 -0500

    ensure irb's completion doesn't interfere with ripl's

--- lib/ripl/completion.rb
+++ lib/ripl/completion.rb
@@ -2,8 +2,8 @@ require 'bond'
 
 module Ripl::Completion
   def before_loop
-    Bond.start(config[:completion] || {})
     super
+    Bond.restart(config[:completion] || {})
   end
 end
 Ripl::Shell.send :include, Ripl::Completion
