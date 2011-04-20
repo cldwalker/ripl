a2f4eea3ebc72b640fe9911dd06d9d5896253779
Lin Jen-Shin <godfat@godfat.org>
2011-04-20 16:21:57 +0800

    readline.rb: See last commit, this one is depending on it.

--- lib/ripl/readline.rb
+++ lib/ripl/readline.rb
@@ -3,6 +3,6 @@ module Ripl::Readline
     Readline.readline prompt, true
   end
 
-  def history; Readline::HISTORY; end
+  def before_loop; @history = Readline::HISTORY; end
 end
 Ripl::Shell.include Ripl::Readline
