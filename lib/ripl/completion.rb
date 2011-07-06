5988ac3eb609e405bba0024b665bb08f71f8f3c6
Postmodern <postmodern.mod3@gmail.com>
2011-07-05 21:14:56 -0700

    Added the Ripl::Completion.complete metod to queue completions for after Bond.restart is called.

--- lib/ripl/completion.rb
+++ lib/ripl/completion.rb
@@ -1,10 +1,34 @@
 require 'bond'
 
 module Ripl::Completion
+  @@completions = []
+
+  #
+  # Adds a tab-completion rule.
+  #
+  # @param [Hash] options
+  #   Pattern matching options.
+  #
+  # @yield [match]
+  #   The given block will be passed the matched data, and should return
+  #   an Array of possible completions.
+  #
+  # @yieldparam [String, nil] match
+  #   The matched data.
+  #
+  def self.complete(options,&block)
+    @@completions << [options, block]
+    return true
+  end
+
   def before_loop
     super
     (config[:completion][:gems] ||= []).concat Ripl.plugins
     Bond.restart config[:completion]
+
+    @@completions.each do |options,block|
+      Bond.complete(options,&block)
+    end
   end
 end
 Ripl::Shell.include Ripl::Completion
